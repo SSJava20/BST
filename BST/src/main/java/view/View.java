3afde9c3b970d0a104c1e2a4f06059eee6a424e1
GAL <galkovsky92@gmail.com>
2012-04-09 12:40:52 +0300

    Cool style added

--- BST/src/main/java/view/View.java
+++ BST/src/main/java/view/View.java
@@ -150,7 +150,7 @@ public class View extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Metal".equals(info.getName())) {
+                if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
